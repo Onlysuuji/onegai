5fad78f755efa4a4bae4c90a042745bff883f03b
onlysuuji <onlysuuji@outlook.jp>
2026-03-21 04:27:41 +0900

    first commit

---
+++ src/client/java/org/example2/onegai/client/OnegaiClient.java
@@ -0,0 +1,10 @@
+package org.example2.onegai.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OnegaiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/example2/onegai/Onegai.java
@@ -0,0 +1,10 @@
+package org.example2.onegai;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Onegai implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
